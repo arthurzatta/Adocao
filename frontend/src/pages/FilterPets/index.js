a48a3a7da7b7a389a1de84f1d6de708242d5955e
vncscampos <viniolic2018@gmail.com>
2021-03-11 15:14:29 -0300

    arrumando filtragem

--- frontend/src/pages/FilterPets/index.js
+++ frontend/src/pages/FilterPets/index.js
@@ -4,9 +4,9 @@ import RNPickerSelect from 'react-native-picker-select';
 import { RadioButton } from 'react-native-paper';
 import Slider from '@react-native-community/slider';
 import { SubmitButton, FormInput, TLabel, Header, Form, IconBox, IconContainer } from './styles';
-import  Icon  from 'react-native-vector-icons/MaterialIcons';
-import  IconIO  from 'react-native-vector-icons/Ionicons';
-import  IconIsto  from 'react-native-vector-icons/Fontisto';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import IconIO from 'react-native-vector-icons/Ionicons';
+import IconIsto from 'react-native-vector-icons/Fontisto';
 
 import axios from 'axios';
 import api from '../../services/api';
@@ -59,40 +59,39 @@ const Filter = ({ navigation }) => {
   }, [selectedUf]);
 
 
-
   return (
-  <View style={styles.container}>
-    <Form style={styles.form}>
-      <Header>
-        <Text style={styles.title}>Filtrar</Text>
-        <Text style={styles.clean}>Limpar</Text>
-      </Header>
+    <View style={styles.container}>
+      <Form style={styles.form}>
+        <Header>
+          <Text style={styles.title}>Filtrar</Text>
+          <Text style={styles.clean}>Limpar</Text>
+        </Header>
 
-      <TLabel>Nome do dono:</TLabel>
-      <FormInput
-        autoCapitalize="none"
-        autoCorrect={false}
-        placeholder=""
-        value={nameOwner}
-        onChangeText={setNameOwner}
-      />
-      <View>
-        <View style={sliderStyles.sliderLabel}>
-          <TLabel>Distância:</TLabel>
-          <TLabel>{Math.round(distance*100)/100}km</TLabel>
-        </View>
-        <Slider
-          style={sliderStyles.slider}
-          thumbTintColor='#FF93B5'
-          minimumTrackTintColor='#FF93B5'
-          maximumTrackTintColor='#FF93B5'
-          minimumValue={1}
-          maximumValue={10}
-          onValueChange={(value) => setDistance(value)}
+        <TLabel>Nome do dono:</TLabel>
+        <FormInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder=""
+          value={nameOwner}
+          onChangeText={setNameOwner}
+        />
+        <View>
+          <View style={sliderStyles.sliderLabel}>
+            <TLabel>Distância:</TLabel>
+            <TLabel>{Math.round(distance * 100) / 100}km</TLabel>
+          </View>
+          <Slider
+            style={sliderStyles.slider}
+            thumbTintColor='#FF93B5'
+            minimumTrackTintColor='#FF93B5'
+            maximumTrackTintColor='#FF93B5'
+            minimumValue={1}
+            maximumValue={10}
+            onValueChange={(value) => setDistance(value)}
           />
-      </View>
+        </View>
 
-      <View style={styles.pickerContainer}>
+        <View style={styles.pickerContainer}>
           <View>
             <TLabel>Estado:</TLabel>
             <RNPickerSelect
@@ -119,174 +118,163 @@ const Filter = ({ navigation }) => {
               value={selectedCity}
             />
           </View>
-      </View>
-      {/* Tipo */}
-      <View>
-        <TLabel>Tipo:</TLabel>
-        <RNPickerSelect
-          placeholder={{
-            label: '-',
-            value: null
-          }}
-          style={pickerSelectStyles}
-          onValueChange={(value) => setType(value)}
-          items={[
-            { label: 'Cachorro', value: 'cachorro' },
-            { label: 'Gato', value: 'gato' },
-            { label: 'Outros', value: 'outros' },
-          ]}
-          value={type}
+        </View>
+        {/* Tipo */}
+        <View>
+          <TLabel>Tipo:</TLabel>
+          <RNPickerSelect
+            placeholder={{
+              label: '-',
+              value: null
+            }}
+            style={pickerSelectStyles}
+            onValueChange={(value) => setType(value)}
+            items={[
+              { label: 'Cachorro', value: 'cachorro' },
+              { label: 'Gato', value: 'gato' },
+              { label: 'Outros', value: 'outros' },
+            ]}
+            value={type}
           />
-      </View>
-      {/* RadioButton */}
-      <View style={radioButtonStyles.radioContainer}>
-        <TLabel>Sexo:</TLabel>
-        <TLabel>Macho</TLabel>
-        <RadioButton.Item
-          style={radioButtonStyles.radioItems}
-          color={'#FF93B5'}
-          value={'true'}
-          status={sex === 'true' ? 'checked' : 'unchecked'}
-          onPress={() => setSex('true')}
+        </View>
+        {/* RadioButton */}
+        <View style={styles.radioContainer}>
+          <TLabel>Sexo:</TLabel>
+          <TLabel>Macho</TLabel>
+          <RadioButton.Item
+            color={'#FF93B5'}
+            value={'true'}
+            style={{marginTop: 15}}
+            status={sex === 'true' ? 'checked' : 'unchecked'}
+            onPress={() => setSex('true')}
+            />
+          <TLabel>Fêmea</TLabel>
+          <RadioButton.Item
+            color={'#FF93B5'}
+            style={{marginTop: 15}}
+            value={'false'}
+            status={sex === 'false' ? 'checked' : 'unchecked'}
+            onPress={() => setSex('false')}
           />
-        <TLabel>Fêmea:</TLabel>
-        <RadioButton.Item
-          color={'#FF93B5'}
-          value={'false'}
-          status={sex === 'false' ? 'checked' : 'unchecked'}
-          onPress={() => setSex('false')}
-        />
-      </View>
+        </View>
 
-      <IconContainer>
-        {/* Vacinado Icon */}
-        <IconBox
-          onPress={() => (!vacinado ? setVacinado(true): setVacinado(false))}
-          style={{borderColor: !vacinado ? '#A4A4A4' : '#FF93B5',}}
-        >
-          <IconIsto name='injection-syringe' style={[iconsStyle.icons, {color: !vacinado ? '#A4A4A4' :'#FF93B5'}]} />
-          <Text style={[iconsStyle.text, {color: !vacinado ? '#A4A4A4' :'#FF93B5'}]}>Vacinado</Text>
-        </IconBox>
+        <IconContainer>
+          {/* Vacinado Icon */}
+          <IconBox
+            onPress={() => (!vacinado ? setVacinado(true) : setVacinado(false))}
+            style={{ borderColor: !vacinado ? '#A4A4A4' : '#FF93B5', }}
+          >
+            <IconIsto name='injection-syringe' style={[iconsStyle.icons, { color: !vacinado ? '#A4A4A4' : '#FF93B5' }]} />
+            <Text style={[iconsStyle.text, { color: !vacinado ? '#A4A4A4' : '#FF93B5' }]}>Vacinado</Text>
+          </IconBox>
 
-        {/* Castrado Icon */}
-        <IconBox
-          onPress={() => (!castrado ? setCastrado(true): setCastrado(false))}
-          style={{borderColor: !castrado ? '#A4A4A4' : '#FF93B5',}}
-        >
-          <Icon name='pets' style={[iconsStyle.icons, {color: !castrado ? '#A4A4A4' :'#FF93B5'}]}/>
-          <Text style={[iconsStyle.text, {color: !castrado ? '#A4A4A4' :'#FF93B5'}]}>Castrado</Text>
-        </IconBox>
+          {/* Castrado Icon */}
+          <IconBox
+            onPress={() => (!castrado ? setCastrado(true) : setCastrado(false))}
+            style={{ borderColor: !castrado ? '#A4A4A4' : '#FF93B5', }}
+          >
+            <Icon name='pets' style={[iconsStyle.icons, { color: !castrado ? '#A4A4A4' : '#FF93B5' }]} />
+            <Text style={[iconsStyle.text, { color: !castrado ? '#A4A4A4' : '#FF93B5' }]}>Castrado</Text>
+          </IconBox>
 
-        {/* Vermifugado Icon */}
-        <IconBox
-          onPress={() => (!vermifugado ? setVermifugado(true): setVermifugado(false))}
-          style={{borderColor: !vermifugado ? '#A4A4A4' : '#FF93B5',}}
-        >
-          <IconIsto name='drug-pack' style={[iconsStyle.icons, {color: !vermifugado ? '#A4A4A4' :'#FF93B5'}]}/>
-          <Text style={[iconsStyle.text, {color: !vermifugado ? '#A4A4A4' :'#FF93B5'}]}>Vermifugado</Text>
-        </IconBox>
+          {/* Vermifugado Icon */}
+          <IconBox
+            onPress={() => (!vermifugado ? setVermifugado(true) : setVermifugado(false))}
+            style={{ borderColor: !vermifugado ? '#A4A4A4' : '#FF93B5', }}
+          >
+            <IconIsto name='drug-pack' style={[iconsStyle.icons, { color: !vermifugado ? '#A4A4A4' : '#FF93B5' }]} />
+            <Text style={[iconsStyle.text, { color: !vermifugado ? '#A4A4A4' : '#FF93B5' }]}>Vermifugado</Text>
+          </IconBox>
 
-        {/* Chipado Icon */}
-        <IconBox
-          onPress={() => (!chipado ? setChipado(true): setChipado(false))}
-          style={{borderColor: !chipado ? '#A4A4A4' : '#FF93B5',}}
-        >
-          <IconIO name='hardware-chip-outline' style={[iconsStyle.icons, {color: !chipado ? '#A4A4A4' :'#FF93B5'}]} />
-          <Text style={[iconsStyle.text, {color: !chipado ? '#A4A4A4' :'#FF93B5'}]}>Chipado</Text>
-        </IconBox>
+          {/* Chipado Icon */}
+          <IconBox
+            onPress={() => (!chipado ? setChipado(true) : setChipado(false))}
+            style={{ borderColor: !chipado ? '#A4A4A4' : '#FF93B5', }}
+          >
+            <IconIO name='hardware-chip-outline' style={[iconsStyle.icons, { color: !chipado ? '#A4A4A4' : '#FF93B5' }]} />
+            <Text style={[iconsStyle.text, { color: !chipado ? '#A4A4A4' : '#FF93B5' }]}>Chipado</Text>
+          </IconBox>
 
-      </IconContainer>
+        </IconContainer>
 
-      <SubmitButton style={styles.button}>
-        <Text style={styles.buttonText}>Filtrar</Text>
-      </SubmitButton>
-    </Form>
-  </View>
-  )};
+        <SubmitButton style={styles.button}>
+          <Text style={styles.buttonText}>Filtrar</Text>
+        </SubmitButton>
+      </Form>
+    </View>
+  )
+};
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontFamily: 'Roboto-Medium',
+    fontSize: 18,
+    color: '#4B4B4B',
+  },
+  clean: {
+    fontFamily: 'Roboto-Medium',
+    fontSize: 18,
+    color: '#FF93B5'
+  },
+  pickerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  radioContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  buttonText: {
+    paddingTop: 11,
+    color: '#fff',
+    fontSize: 24,
+    fontFamily: 'Ubuntu-Bold',
+    textAlign: 'center',
+  },
+  button: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 8,
     },
-    title: {
-      fontFamily: "Ubuntu-Bold",
-      fontSize: 18,
-      color: '#4B4B4B',
-    },
-    clean: {
-      fontFamily: "Ubuntu",
-      fontSize: 18,
-      color: '#FF93B5'
-    },
-    pickerContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-    },
-    form: {
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 12,
-      },
-      shadowOpacity: 0.58,
-      shadowRadius: 16.00,
-      elevation: 24,
-    },
-    buttonText: {
-      paddingTop: 11,
-      color: '#fff',
-      fontSize: 24,
-      fontFamily: 'Ubuntu-Bold',
-      textAlign: 'center',
-    },
-    button: {
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 8,
-      },
-      shadowOpacity: 0.16,
-      elevation: 3,
-    },
+    shadowOpacity: 0.16,
+    elevation: 3,
+  },
 
-  });
+});
 
-  const iconsStyle = StyleSheet.create({
-    icons: {
-      alignSelf: 'center',
-      fontSize: 50,
-    },
-    text: {
-      alignSelf: 'center',
-      fontFamily: 'Ubuntu',
-    }
-  })
+const iconsStyle = StyleSheet.create({
+  icons: {
+    alignSelf: 'center',
+    fontSize: 31,
+  },
+  text: {
+    alignSelf: 'center',
+    fontFamily: 'Ubuntu',
+    fontSize: 12
+  }
+})
 
-  const radioButtonStyles = StyleSheet.create({
-    radioContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-    },
-    radioItems: {
-      alignSelf: 'center'
-    }
-  });
+const sliderStyles = StyleSheet.create({
+  slider: {
+    padding: 20,
+  },
+  sliderLabel: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginRight: 15,
+  }
+});
 
-  const sliderStyles = StyleSheet.create({
-    slider:{
-      padding: 20,
-    },
-    sliderLabel:{
-      flexDirection: 'row',
-      justifyContent: 'space-between'
-    }
-  });
-
-  const pickerSelectStyles = StyleSheet.create({
-    inputAndroid: {
-      width: 177,
-      color: "#a4a4a4",
-    },
-  });
+const pickerSelectStyles = StyleSheet.create({
+  inputAndroid: {
+    width: 177,
+    color: "#a4a4a4",
+  },
+});
 
 export default Filter;
