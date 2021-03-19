fae364aca0577ab4a86378059898b2b60ddfb01d
arthurzatta <arthur.zatta@gmail.com>
2021-03-19 14:27:05 -0300

    Detalhes incrementados

--- frontend/src/pages/Favorites/index.js
+++ frontend/src/pages/Favorites/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Text, FlatList, View, StyleSheet} from 'react-native';
+import { Text, FlatList, View, StyleSheet, Image} from 'react-native';
 import Background from '../../Components/Background/index';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { TouchableOpacity } from 'react-native-gesture-handler';
@@ -30,36 +30,41 @@ export default function Favorites({navigation}) {
           <Icon name="arrow-left-thick" size={22} color={'#fff'} onPress={() => navigation.pop()} />
 					<Text style={styles.title}>Pets Favoritos</Text>
         </Header> 
-			
+
         <Form style={styles.form}>
+        { pets.length !== 0  ? (
           <FlatList 
             data={pets}
             style={styles.notificationList}
             renderItem={({ item: pet }) => (
               <>
-              <TouchableOpacity style={styles.boxNotification} onPress={() => navigation.navigate('DescriptionPet', {id: pet.id})}>
-                <View style={{ flexDirection: 'row'}}>
-                  <View style={styles.viewImg}>
-                    <Img source={{uri: pet.image}}/>
+                <TouchableOpacity style={styles.boxNotification} onPress={() => navigation.navigate('DescriptionPet', {id: pet.id})}>
+                    <View style={{ flexDirection: 'row'}}>
+                      <View style={styles.viewImg}>
+                        <Img source={{uri: pet.image}}/>
+                      </View>
+                      <View>
+                        <View style={{flexDirection: 'row'}}>
+                          <TitleNotification>{pet.name}</TitleNotification>
+                          {pet.sex == 'm' ? (
+                              <Icon name='gender-male' size={30} color={'#78CEFF'} style={{marginLeft: 5}}/>
+                            ) : (
+                              <Icon name='gender-female' size={30} color={'#FF93B5'} style={{marginLeft: 5}}/>
+                              )}
+                        </View>
+                        <Description>{pet.description}</Description>
+                      </View>
                   </View>
-                  <View>
-                    <View style={{flexDirection: 'row'}}>
-                      <TitleNotification>{pet.name}</TitleNotification>
-                      {pet.sex == 'm' ? (
-                          <Icon name='gender-male' size={30} color={'#78CEFF'} style={{marginLeft: 5}}/>
-                        ) : (
-                          <Icon name='gender-female' size={30} color={'#FF93B5'} style={{marginLeft: 5}}/>
-                          )}
-                    </View>
-                    <Description>{pet.description}</Description>
-                  </View>
-              </View>
-            </TouchableOpacity>
+                </TouchableOpacity>
               </>
             )}
-          >
+            />) : (
+              <View style={{flex: 1, flexDirection:  'column', marginTop: 250}}>
+                <Image source={require('../../assets/cat.png')} style={{alignSelf: 'center'}}/>
+                <Text style={styles.noFavsText}>Você não possui favoritos</Text>
+              </View>
+            )}
             
-          </FlatList>
         </Form>		
     </Background>	
   )
@@ -114,5 +119,11 @@ const styles = StyleSheet.create({
     height: 55,
     borderRadius: 55,
     elevation: 0.5
+  },
+  noFavsText: {
+    fontFamily: 'Ubuntu-Bold',
+    color: '#000',
+    fontSize: 18,
+    alignSelf: 'center',
   }
 });
--- frontend/src/pages/ResgiteredPets/index.js
+++ frontend/src/pages/ResgiteredPets/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Text, FlatList, View, StyleSheet, ScrollView } from 'react-native';
+import { Text, FlatList, View, StyleSheet, Image } from 'react-native';
 import Background from '../../Components/Background/index';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { TouchableOpacity } from 'react-native-gesture-handler';
@@ -32,33 +32,40 @@ export default function RegisteredPet({navigation}) {
         </Header> 
 			
         <Form style={styles.form}>
-          <FlatList 
-            data={pets}
-            style={styles.notificationList}
-            renderItem={({ item: pet }) => (
-              <>
-              <TouchableOpacity style={styles.boxNotification} onPress={() => navigation.navigate('DescriptionPet', {id: pet.id})}>
-                <View style={{ flexDirection: 'row'}}>
-                  <View style={styles.viewImg}>
-                    <Img source={{uri: pet.image}}/>
-                  </View>
-                  <View>
-                    <View style={{flexDirection: 'row'}}>
-                      <TitleNotification>{pet.name}</TitleNotification>
-                      {pet.sex == 'm' ? (
-                          <Icon name='gender-male' size={30} color={'#78CEFF'} style={{marginLeft: 5, marginTop: 9}}/>
-                        ) : (
-                          <Icon name='gender-female' size={30} color={'#FF93B5'} style={{marginLeft: 5, marginTop: 9}}/>
-                          )}
+
+          {pets.length !== 0 
+          ? (
+            <FlatList 
+              data={pets}
+              style={styles.notificationList}
+              renderItem={({ item: pet }) => (
+                <>
+                <TouchableOpacity style={styles.boxNotification} onPress={() => navigation.navigate('DescriptionPet', {id: pet.id})}>
+                  <View style={{ flexDirection: 'row'}}>
+                    <View style={styles.viewImg}>
+                      <Img source={{uri: pet.image}}/>
+                    </View>
+                    <View>
+                      <View style={{flexDirection: 'row'}}>
+                        <TitleNotification>{pet.name}</TitleNotification>
+                        {pet.sex == 'm' ? (
+                            <Icon name='gender-male' size={30} color={'#78CEFF'} style={{marginLeft: 5, marginTop: 9}}/>
+                          ) : (
+                            <Icon name='gender-female' size={30} color={'#FF93B5'} style={{marginLeft: 5, marginTop: 9}}/>
+                            )}
+                      </View>
                     </View>
                   </View>
-              </View>
-            </TouchableOpacity>
-              </>
-            )}
-          >
+                </TouchableOpacity>
+                </>
+              )}
+            />) : (
+            <View style={{flex: 1, flexDirection:  'column', marginTop: 250}}>
+              <Image source={require('../../assets/cat.png')} style={{alignSelf: 'center'}}/>
+              <Text style={styles.noFavsText}>Você não possui pets cadastrados</Text>
+            </View>
+          )}
             
-          </FlatList>
         </Form>		
     </Background>	
   )
@@ -113,5 +120,11 @@ const styles = StyleSheet.create({
     height: 55,
     borderRadius: 55,
     elevation: 0.5
+  },
+  noFavsText: {
+    fontFamily: 'Ubuntu-Bold',
+    color: '#000',
+    fontSize: 18,
+    alignSelf: 'center',
   }
 });
