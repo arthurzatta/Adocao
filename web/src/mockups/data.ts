c0a06b19df60dcbe9dcf426daece624feebb4284
arthurzatta <arthur.zatta@gmail.com>
2021-07-25 23:02:02 -0300

    Alguns mockups para teste

---
+++ web/src/mockups/data.ts
@@ -0,0 +1,92 @@
+export const users = [
+    {
+        id: 1,
+        name: 'teste',
+        email: 'teste@teste.com',
+        password: 'teste123',
+        phone: '31988884444',
+        city: 'Teste',
+        state: 'MG',
+        isOng: false
+    },
+    {
+        id: 2,
+        name: 'teste2',
+        email: 'teste2@teste.com',
+        password: 'teste123',
+        phone: '31988884444',
+        city: 'Teste',
+        state: 'MG',
+        isOng: false
+    },
+    {
+        id: 3,
+        name: 'teste3',
+        email: 'teste3@teste.com',
+        password: 'teste123',
+        phone: '31988884444',
+        city: 'Teste',
+        state: 'MG',
+        isOng: true
+    },
+    {
+        id: 4,
+        name: 'teste4',
+        email: 'teste4@teste.com',
+        password: 'teste123',
+        phone: '31988884444',
+        city: 'Teste',
+        state: 'MG',
+        isOng: true
+    },
+];
+
+export const animals = [
+    {
+        name: 'Toto',
+        description: 'teste',
+        gender: 'm',
+        status: {
+            vacinado: true,
+            vermifugado: false,
+            castrado: false,
+            chipado: true
+        },
+        userId: 1
+    },
+    {
+        name: 'Laika',
+        description: 'teste',
+        gender: 'f',status: {
+            vacinado: true,
+            vermifugado: true,
+            castrado: false,
+            chipado: false
+        },
+        userId: 2
+    },
+    {
+        name: 'Totoro',
+        description: 'teste',
+        gender: 'm',
+        status: {
+            vacinado: true,
+            vermifugado: true,
+            castrado: true,
+            chipado: false
+        },
+        userId: 3
+    },
+    {
+        name: 'Dores',
+        description: 'teste',
+        gender: 'f',
+        status: {
+            vacinado: true,
+            vermifugado: false,
+            castrado: false,
+            chipado: false
+        },
+        userId: 4
+    },
+]
